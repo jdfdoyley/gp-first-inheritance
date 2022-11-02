c7d1f014f8d472d2db9176d34c421cfbb9aa9595
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-01 21:09:54 -0400

    Add Shape class code

---
+++ Shape.cs
@@ -0,0 +1,28 @@
+/******************************************************************************\
+* Name: Jason D. F. D'Oyley
+* Date: November 01, 2022
+* Assignment CIS214 Week 5 GP - First Inheritance
+*
+* Shape Class.
+* This is the base class that provides the generic information that can be
+* shared by all classes that inherit from it. In this case, the generic
+* information is the shape's color - generic because in our example, all shapes
+* will have a color.
+\******************************************************************************/
+public class Shape
+{
+    // Class property
+    public string Color { get; set; }
+
+    // Constructor; sets the class's color property
+    public Shape(string color)
+    {
+        Color = color;
+    }
+
+    // Function to print the Shape's information
+    public void PrintShape()
+    {
+        Console.WriteLine("The Shape's color is {0}.\n", Color);
+    }
+}
