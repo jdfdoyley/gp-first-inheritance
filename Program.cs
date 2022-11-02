712efc82624406f8cdbfa4107802ee9c72b8390f
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-01 21:22:39 -0400

    Add readme content and header text

--- Program.cs
+++ Program.cs
@@ -2,4 +2,19 @@
 * Name: Jason D. F. D'Oyley
 * Date: November 01, 2022
 * Assignment CIS214 Week 5 GP - First Inheritance
+*
+* Main application (program) class.
+* In this application I will demonstrate the concept of inheritance by
+* instantiating Shape and Rectangle objects and demonstratin the use of base
+* class methods in a derived class instantiated object.
 \******************************************************************************/
+public class ShapeDemo
+{
+    static void Main(string[] args)
+    {
+        // Print a header line
+        Console.WriteLine(
+            "Jason D'Oyley - Week 5 GP - First Inheritance\n"
+        );
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# First Inheritance
+
+In this application I will demonstrate the concept of inheritance
+by instantiating Shape and Rectangle objects and demonstratin the
+use of base class methods in a derived class instantiated object.
